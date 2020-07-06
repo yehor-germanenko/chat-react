8b942a40d9af651b4e1462404c451119ef4b23b3
Git-horse <47929284+Git-horse@users.noreply.github.com>
2020-07-06 17:25:21 +0300

    Updates

---
+++ precache-manifest.1ac4bbebdbbe99988804199d71a38c29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd93ea82d51f78e27fb530ae85afe26",
+    "url": "/chat-react/index.html"
+  },
+  {
+    "revision": "0292206067f18753338f",
+    "url": "/chat-react/static/css/main.b019e4eb.chunk.css"
+  },
+  {
+    "revision": "fba6fd5b274188f5c34c",
+    "url": "/chat-react/static/js/2.c9ecd03f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chat-react/static/js/2.c9ecd03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0292206067f18753338f",
+    "url": "/chat-react/static/js/main.3b4bba81.chunk.js"
+  },
+  {
+    "revision": "5fae231505e17469a89f",
+    "url": "/chat-react/static/js/runtime-main.b4ea5d6b.js"
+  },
+  {
+    "revision": "9ce3a47296ec5b0ffe5d44bfdc6f3907",
+    "url": "/chat-react/static/media/send.9ce3a472.svg"
+  },
+  {
+    "revision": "bbf5793f9e012fbde04b73cf21f51e44",
+    "url": "/chat-react/static/media/wolf.bbf5793f.png"
+  }
+]);
